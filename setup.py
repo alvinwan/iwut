398fb6acff00361fce006f48178f9aebb1de1b3b
Alvin Wan <alvinwan1@gmail.com>
2021-10-26 15:39:44 -0700

    css fixes for jupyterlab

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description =  f.read()
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 setuptools.setup(
     name="iwut",
