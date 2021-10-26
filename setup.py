eda2c5ff590d318aff459bd843e06bd674bef792
Alvin Wan <alvinwan1@gmail.com>
2021-10-26 14:18:04 -0700

    version bump to 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description =  f.read()
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 setuptools.setup(
     name="iwut",
